d755c069867a0d22a9acc062e5e90c33ad748a5d
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-21 20:12:14 -0400

    commit tree 8b88f661f0a33db255b59ada6e10b4669198a188

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+
+function submitData(name, email){
+    return fetch = fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+          })
+    })
+
+    .then(response => response.json())
+    .then(object => {
+        document.body.innerHTML = object["id"]
+        debugger
+    })
+    .catch(function(error) {
+        // alert("ERROR!")
+        // console.log(error.message)
+        document.body.innerHTML = error.message
+      })
+
+}
